88c7f5cc65bd4ce4f4caa0cdec42b3b171d4bb50
Swarup Sapkota <empswarupcoc@gmail.com>
2024-01-08 09:46:39 +0545

    todo component imported to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import styles from "./page.module.css";
+import Todo from "./components/todo";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Todo />
+    </main>
+  );
 }
