45d08dc518079f1388561a3653397916a46e1dc4
Swarup Sapkota <empswarupcoc@gmail.com>
2024-01-25 20:28:33 +0545

    build fix #1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,12 +14,16 @@ const auth = NextAuth({
     }),
   ],
   callbacks: {
-    session: ({ session }) => ({
-      ...session,
-      user: findUser(session.user?.name),
-    }),
+    async session({ session, token }) {
+      const user = await getUserOnServer();
+      return {
+        ...session,
+        user,
+      };
+    },
   },
 });
+
 export { auth as GET, auth as POST };
 
 const validUsers: UserInfo[] = [
@@ -31,7 +35,7 @@ function findUser(name?: string | null) {
   return validUsers.find((user) => user.name === name);
 }
 
-export async function getUserOnServer() {
+async function getUserOnServer() {
   const session = await getServerSession();
   return findUser(session?.user?.name);
 }
